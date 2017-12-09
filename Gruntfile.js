9111385e7ff9c6b49a814a5d1ed9ce89d37e6465
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-12-08 23:55:45 -0200

    Compiling with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        files: {
+          "css/estilos.css": "css/estilos.less", // Caminho dos arquivos
+          "css/media-queries.css": "css/media-queries.less", // Caminho dos arquivos
+          "css/normalize.css": "css/normalize.less" // Caminho dos arquivos
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['**/*.less'], // Quais arquivos o grunt ficará de olho
+        tasks: ['less']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
